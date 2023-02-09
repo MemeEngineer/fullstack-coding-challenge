507f459d33a4e2837b55bdfbff3dbc3137930568
Johnny Wu <johnnyw131@gmail.com>
2023-02-09 16:21:23 -0500

    Table progress

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -27,24 +27,39 @@ console.log(userComplaints)
 
     return(
     <div>
+<table key={userComplaints.uniquekey}>
+<tr>
+    <th>Account</th>
+    <th>Borough</th>
+    <th>City</th>
+    <th>Close Date</th>
+    <th>Community Board</th>
+    <th>Complaint Type</th>
+    <th>Council District</th>
+    <th>Descriptor</th>
+    <th>Open Date</th>
+    <th>UniqueKey</th>
+    <th>Zip Code</th>
+  </tr>
 {userComplaints.map((complaint) =>{ return(
-  <div key={complaint.uniquekey}>
-  <p>{complaint.account}</p>
-  </div>
-  // {complaint.borough}
-  // {complaint.city}
-  // {complaint.closedate}
-  // {complaint.community_board}
-  // {complaint.complaint_type}
-  // {complaint.council_dist}
-  // {complaint.descriptor}
-  // {complaint.opendate}
-  // {complaint.uniquekey}
-  // {complaint.zip}
-)}
-)}
+
+  <tr>
+  <td>{complaint.account}</td>
+  <td>{complaint.borough}</td>
+  <td>{complaint.city}</td>
+  <td>{complaint.closedate}</td>
+  <td>{complaint.community_board}</td>
+  <td>{complaint.complaint_type}</td>
+  <td>{complaint.council_dist}</td>
+  <td>{complaint.descriptor}</td>
+  <td>{complaint.opendate}</td>
+  <td>{complaint.uniquekey}</td>
+  <td>{complaint.zip}</td>
+  </tr>
+)})}
+</table>
 </div>
-    )
+)
 };
 
 export default Dashboard;
