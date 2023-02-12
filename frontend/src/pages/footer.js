c38de17fc62d60b366497352a31ad39c6e791090
Johnny Wu <johnnyw131@gmail.com>
2023-02-12 03:06:07 -0500

    Footer added

---
+++ frontend/src/pages/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <div style={{display: "flex", justifyContent: "center"}}>
+        <p> © 2023 | 🗽 New York City Council 🏛️|  All Rights Reserved</p>
+        </div>
+    )
+};
+
+export default Footer;
--- frontend/src/pages/mainpage.js
+++ frontend/src/pages/mainpage.js
@@ -2,6 +2,7 @@ import React from "react";
 // import { BrowserRouter as Router} from "react-router-dom";
 import Dashboard from "./dashboard.js";
 import Header from "./header.js";
+import Footer from "./footer.js";
 
 function Mainpage({userToken,setUserToken}){
 
@@ -13,6 +14,7 @@ function Mainpage({userToken,setUserToken}){
       userToken={userToken}
       setUserToken={setUserToken}
       />
+      <Footer/>
     </div>
   );
 }
